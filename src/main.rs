25c18ea5fc7878b9d1d07ce83690f964e5c5a149
fgdou <fabigoardou@gmail.com>
2023-11-22 14:39:55 +0100

    header

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,59 @@
 use yew::prelude::*;
 
+#[derive(Properties, PartialEq)]
+pub struct HeaderButtonProps {
+    #[prop_or_default]
+    selected: bool,
+    name: &'static str,
+    #[prop_or_default]
+    callback: Option<Callback<&'static str>>,
+}
+
+#[function_component]
+fn HeaderButton(props: &HeaderButtonProps) -> Html {
+
+    let name = props.name;
+
+    let onclick = match props.callback.clone() {
+        Some(callback) => {
+            Callback::from(move |_| {
+                callback.emit(name);
+            })
+        },
+        None => Callback::from(|_|{})
+    };
+
+    html! {
+        <div {onclick} class={if props.selected {"header-btn active"} else {"header-btn"}}>
+            {props.name}
+        </div>
+    }
+}
+
+#[function_component]
+fn Header() -> Html {
+    let buttons = ["Home", "Experience", "Project", "Contact"];
+    let active = use_state(buttons[0].to_string());
+
+    let elements: Html = buttons.iter().map(|name| {
+        html! {
+            <HeaderButton {name} selected={*name == active} />
+        }
+    }).collect();
+
+    html! {
+        <header>
+            {elements}
+        </header>
+    }
+}
+
 #[function_component]
 fn App() -> Html {
     html! {
-        <>
-            {"Hello world"}
-        </>
+        <div>
+            <Header/>
+        </div>
     }
 }
 
