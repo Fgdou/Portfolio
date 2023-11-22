ba48c673cb89e01c97a414c9bcdba417b870898d
Fgdou <fabigoardou@gmail.com>
2023-11-22 15:17:35 +0100

    refactor files

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,7 @@
+mod components;
 use yew::prelude::*;
 
-#[derive(Properties, PartialEq)]
-pub struct HeaderButtonProps {
-    #[prop_or_default]
-    selected: bool,
-    name: &'static str,
-    #[prop_or_default]
-    callback: Option<Callback<&'static str>>,
-}
-
-#[function_component]
-fn HeaderButton(props: &HeaderButtonProps) -> Html {
-
-    let name = props.name;
-
-    let onclick = match props.callback.clone() {
-        Some(callback) => {
-            Callback::from(move |_| {
-                callback.emit(name);
-            })
-        },
-        None => Callback::from(|_|{})
-    };
-
-    html! {
-        <div {onclick} class={if props.selected {"header-btn active"} else {"header-btn"}}>
-            {props.name}
-        </div>
-    }
-}
-
-#[function_component]
-fn Header() -> Html {
-    let buttons = ["Home", "Experience", "Project", "Contact"];
-    let active = use_state(|| buttons[0]);
-
-    let elements: Html = buttons.iter().map(|name| {
-        let active = active.clone();
-        html! {
-            <HeaderButton {name} selected={*name == *active} callback={move |s| active.set(s)} />
-        }
-    }).collect();
-
-    html! {
-        <header>
-            {elements}
-        </header>
-    }
-}
+use crate::components::Header;
 
 #[function_component]
 fn App() -> Html {
