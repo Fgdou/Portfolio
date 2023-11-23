cc6e073e14ac7d2e9f53a4375294ab9c994ecbca
Fgdou <fabigoardou@gmail.com>
2023-11-23 14:22:57 +0100

    move data

---
+++ src/data.rs
@@ -0,0 +1,85 @@
+use std::rc::Rc;
+
+use yew::prelude::*;
+
+use crate::components::*;
+
+pub struct Data {
+    pub home_props: HomeProps,
+    pub experiences: ExperienceData
+}
+
+impl Default for Data {
+    fn default() -> Self {
+        Self { 
+            home_props: HomeProps{
+                fonction: AttrValue::from("Software Engineer"),
+                name: AttrValue::from("Fabien GOARDOU"),
+                logo_list: vec!(
+                    LogoListProps{
+                        logos: vec!(
+                            LogoProps{
+                                name: AttrValue::from("Rust"),
+                                path: AttrValue::from("assets/img/rust.svg")
+                            },
+                            LogoProps{
+                                name: AttrValue::from("C++"),
+                                path: AttrValue::from("assets/img/cpp.svg")
+                            },
+                            LogoProps{
+                                name: AttrValue::from("Java"),
+                                path: AttrValue::from("assets/img/java.svg")
+                            },
+                            LogoProps{
+                                name: AttrValue::from("Typescript"),
+                                path: AttrValue::from("assets/img/typescript.svg")
+                            },
+                        ),
+                        title: AttrValue::from("Languages")
+                    },
+                    LogoListProps{
+                        logos: vec!(
+                            LogoProps{
+                                name: AttrValue::from("Linux"),
+                                path: AttrValue::from("assets/img/linux.svg"),
+                            },
+                            LogoProps{
+                                name: AttrValue::from("NodeJS"),
+                                path: AttrValue::from("assets/img/nodejs.svg"),
+                            },
+                            LogoProps{
+                                name: AttrValue::from("React"),
+                                path: AttrValue::from("assets/img/react.svg"),
+                            },
+                            LogoProps{
+                                name: AttrValue::from("Angular"),
+                                path: AttrValue::from("assets/img/angular.svg"),
+                            },
+                            LogoProps{
+                                name: AttrValue::from("AWS"),
+                                path: AttrValue::from("assets/img/aws.svg"),
+                            },
+                        ),
+                        title: AttrValue::from("Techologies")
+                    },
+                )
+            },
+            experiences: ExperienceData{
+                jobs: vec!(
+                    Rc::from(ExperienceSingleData{
+                        image: AttrValue::from("assets/img/amazon.svg"),
+                        role: AttrValue::from("Software Developer Engineer"),
+                        start: AttrValue::from("June 2023"),
+                        duration: AttrValue::from("4 Months"),
+                        location: AttrValue::from("Cambridge, UK"),
+                        skills: ["AI", "Python", "HuggingFace", "AWS"].into_iter().map(AttrValue::from).collect(),
+                        softskills: ["Team Work", "SCRUM", "Communication"].into_iter().map(AttrValue::from).collect(),
+                    })
+                ),
+                schools: vec!(
+        
+                )
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,23 @@
 mod components;
+mod data;
 use std::rc::Rc;
 
-use yew::{prelude::*, props};
+use yew::prelude::*;
 
-use crate::components::*;
+use components::*;
+use data::*;
 
 #[function_component]
 fn App() -> Html {
 
-    let root = props!(
-        HomeProps{
-            fonction: AttrValue::from("Software Engineer"),
-            name: AttrValue::from("Fabien GOARDOU"),
-            logo_list: vec!(
-                LogoListProps{
-                    logos: vec!(
-                        LogoProps{
-                            name: AttrValue::from("Rust"),
-                            path: AttrValue::from("assets/img/rust.svg")
-                        },
-                        LogoProps{
-                            name: AttrValue::from("C++"),
-                            path: AttrValue::from("assets/img/cpp.svg")
-                        },
-                        LogoProps{
-                            name: AttrValue::from("Java"),
-                            path: AttrValue::from("assets/img/java.svg")
-                        },
-                        LogoProps{
-                            name: AttrValue::from("Typescript"),
-                            path: AttrValue::from("assets/img/typescript.svg")
-                        },
-                    ),
-                    title: AttrValue::from("Languages")
-                },
-                LogoListProps{
-                    logos: vec!(
-                        LogoProps{
-                            name: AttrValue::from("Linux"),
-                            path: AttrValue::from("assets/img/linux.svg"),
-                        },
-                        LogoProps{
-                            name: AttrValue::from("NodeJS"),
-                            path: AttrValue::from("assets/img/nodejs.svg"),
-                        },
-                        LogoProps{
-                            name: AttrValue::from("React"),
-                            path: AttrValue::from("assets/img/react.svg"),
-                        },
-                        LogoProps{
-                            name: AttrValue::from("Angular"),
-                            path: AttrValue::from("assets/img/angular.svg"),
-                        },
-                        LogoProps{
-                            name: AttrValue::from("AWS"),
-                            path: AttrValue::from("assets/img/aws.svg"),
-                        },
-                    ),
-                    title: AttrValue::from("Techologies")
-                },
-            )
-        }
-    );
+    let data = Data::default();
 
-    let experiences = Rc::from(ExperienceData{
-        jobs: vec!(
-            Rc::from(ExperienceSingleData{
-                image: AttrValue::from("assets/img/amazon.svg"),
-                role: AttrValue::from("Software Developer Engineer"),
-                start: AttrValue::from("June 2023"),
-                duration: AttrValue::from("4 Months"),
-                location: AttrValue::from("Cambridge, UK"),
-                skills: ["AI", "Python", "HuggingFace", "AWS"].into_iter().map(AttrValue::from).collect(),
-                softskills: ["Team Work", "SCRUM", "Communication"].into_iter().map(AttrValue::from).collect(),
-            })
-        ),
-        schools: vec!(
-
-        )
-    });
+    let experiences = Rc::from(data.experiences);
 
     html! {
         <div>
             <Header/>
-            <Home ..root />
+            <Home ..data.home_props />
             <Experience data={experiences} />
         </div>
     }
